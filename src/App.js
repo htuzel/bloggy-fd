a0c9a0dadf30def3570fd88a1c119b8bfc5b1788
Hayreddin Tüzel <hayreddintuzel@gmail.com>
2021-04-24 01:25:24 +0200

    repo initialization.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function register() {
+    const data = {
+      name,
+      email,
+      password
+    };
+
+    fetch('http://localhost:5000/api/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  }
+
+  function handleChange(e) {
+    if (e.target.name === 'name') {
+      setName(e.target.value);
+    } else if (e.target.name === 'password') {
+      setPassword(e.target.value);
+    } else if (e.target.name === 'email') {
+      setEmail(e.target.value);
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input value={name} onChange={handleChange} type="text" name="name"></input>
+        <input value={email} onChange={handleChange} type="text" name="email"></input>
+        <input value={password} onChange={handleChange} type="password" name="password"></input>
+        <button onClick={register}>Register</button>
+      </div>
     </div>
   );
 }
